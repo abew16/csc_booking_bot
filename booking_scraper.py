aecb86717e18e0b63b714f03f6e863ade9af7b88
Abe Waziri <abe.waziri@gmail.com>
2026-02-01 12:26:56 -0500

    updated chromium options

--- booking_scraper.py
+++ booking_scraper.py
@@ -32,6 +32,7 @@ class BookingScraper:
             chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
         chrome_options.add_argument('--disable-dev-shm-usage')
+        chrome_options.add_argument('--remote-debugging-pipe')
         chrome_options.add_argument('--disable-blink-features=AutomationControlled')
         chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
         chrome_options.add_experimental_option('useAutomationExtension', False)
